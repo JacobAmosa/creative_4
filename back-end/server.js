91cf0124f6f932de5841fc1274fc7e43b2d9bac8
Jacob Amosa <jacobamosa@gmail.com>
2021-04-03 15:14:31 -0600

    server.js

--- back-end/server.js
+++ back-end/server.js
@@ -18,4 +18,69 @@ mongoose.connect('mongodb://localhost:27017/todo', {
   useUnifiedTopology: true
 });
 
+// Create a newly assembled bike. Takes suspension, frame, and tire type.
+app.post('/api/bikes', async (req, res) => {
+  const bike = new Bike({
+    suspension: req.body.suspension,
+    frame: req.body.frame,
+    tires: req.body.tires,
+  });
+  try {
+    console.log(req.body);
+    await bike.save();
+    res.send(bike);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Create a scheme for fully assembled bikes.
+const bikeSchema = new mongoose.Schema({
+  suspension: String,
+  frame: String,
+  tires: String,
+});
+
+// Create a model for assembled bikes.
+const Bike = mongoose.model('Bike', bikeSchema);
+
+// Gets a list of all assembled bikes.
+app.get('/api/bikes', async (req, res) => {
+  try {
+    let bikes = await Bike.find();
+    res.send(bikes);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+//Retrieves the specific ID of bike and deletes.
+app.delete('/api/bikes/:id', async (req, res) => {
+  try{
+    await Bike.deleteOne(req.bike);
+    res.sendStatus(200);
+  }catch (error) {
+  console.log(error);
+  res.sendStatus(500);
+  }
+});
+
+//Retrieves and edits specific bike.
+app.put('/api/bikes/:id', async (req, res) => {
+  let editedbike;
+  try{
+    editedbike = await Bike.findOne({_id:req.params.id});
+    editedbike.suspension = req.body.suspension;
+    editedbike.frame = req.body.frame;
+    editedbike.tires = req.body.tires;
+    editedbike.save();
+    res.sendStatus(200);
+  }catch (error) {
+  console.log(error);
+  res.sendStatus(500);
+  }
+});
+
 app.listen(3000, () => console.log('Server listening on port 3000!'));
