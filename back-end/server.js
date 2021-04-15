94c0b976f601eb8de2e3573f6d304c93ab1e7848
Jacob Amosa <jacobamosa@gmail.com>
2021-04-14 19:19:43 -0600

    more changes

--- back-end/server.js
+++ back-end/server.js
@@ -19,6 +19,18 @@ mongoose.connect('mongodb://localhost:27017/dreambike', {
   useUnifiedTopology: true
 });
 
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+    name: 'session',
+    keys: ['secretValue'],
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    }
+}));
+
 // Create a newly assembled bike. Takes suspension, frame, and tire type.
 app.post('/api/bikes', async (req, res) => {
   const bike = new Bike({
